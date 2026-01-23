13edfc485d62a955f3b3f28515781aa5fefc0777
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-23 18:40:36 +0200

    chore: add initial migration

---
+++ WebApplication1/Migrations/20260123163936_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Categoria = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Moeda = table.Column<string>(type: "TEXT", nullable: false),
+                    Marca = table.Column<string>(type: "TEXT", nullable: false),
+                    Modelo = table.Column<string>(type: "TEXT", nullable: false),
+                    Ano = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cor = table.Column<string>(type: "TEXT", nullable: false),
+                    ArmazenamentoGB = table.Column<int>(type: "INTEGER", nullable: false),
+                    RamGB = table.Column<int>(type: "INTEGER", nullable: false),
+                    Condicao = table.Column<string>(type: "TEXT", nullable: false),
+                    StockDisponivel = table.Column<int>(type: "INTEGER", nullable: false),
+                    StockReservado = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_Categoria_Marca_Modelo_Preco_Nome_Cor_ArmazenamentoGB_RamGB_Condicao",
+                table: "Produtos",
+                columns: new[] { "Categoria", "Marca", "Modelo", "Preco", "Nome", "Cor", "ArmazenamentoGB", "RamGB", "Condicao" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
