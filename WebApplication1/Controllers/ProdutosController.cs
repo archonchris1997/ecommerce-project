aab454c4ad61d350e7c46a17a863b70729a53fa1
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-23 18:57:39 +0200

    pequenas mudanças no ProdutosController

--- WebApplication1/Controllers/ProdutosController.cs
+++ WebApplication1/Controllers/ProdutosController.cs
@@ -1,6 +1,13 @@
-namespace WebApplication1.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
-public class ProdutosController
+namespace WebApplication1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+[ApiController]
+[Route("api/produtos")]
+public class ProdutosController:ControllerBase
 {
     
 }
