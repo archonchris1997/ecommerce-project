6d84e4676ed20e4dd7be97e1448752547e921c36
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-24 21:05:28 +0200

    CascadeFiltersResponse

--- WebApplication1/Models/Models.DTO/CascadeFiltersResponse.cs
+++ WebApplication1/Models/Models.DTO/CascadeFiltersResponse.cs
@@ -1,3 +1,12 @@
 namespace WebApplication1.Models.Models.DTO;
 
-public record CascadeFiltersResponse();
+public record FilterOption(string Value, int Count);
+
+public record CascadeFiltersResponse(  List<FilterOption> Marcas,
+    List<FilterOption> Modelos,
+    List<FilterOption> Anos,
+    List<FilterOption> Cores,
+    List<FilterOption> Armazenamentos,
+    List<FilterOption> Rams,
+    List<FilterOption> Condicoes
+);
