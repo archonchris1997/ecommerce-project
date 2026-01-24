8b2f4b95e8f38f01171f5a5ff6a4415adab753f4
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-24 22:16:40 +0200

    alteracao de CascadeFiltersResponseDto

--- WebApplication1/Models/Models.DTO/CascadeFiltersResponseDto.cs
+++ WebApplication1/Models/Models.DTO/CascadeFiltersResponseDto.cs
@@ -2,5 +2,11 @@ namespace WebApplication1.Models.Models.DTO;
 
 public class CascadeFiltersResponseDto
 {
-    
+    public List<FilterOptionDto> Marcas { get; set; } = [];
+    public List<FilterOptionDto> Modelos { get; set; } = [];
+    public List<FilterOptionDto> Anos { get; set; } = [];
+    public List<FilterOptionDto> Cores { get; set; } = [];
+    public List<FilterOptionDto> Armazenamentos { get; set; } = [];
+    public List<FilterOptionDto> Rams { get; set; } = [];
+    public List<FilterOptionDto> Condicoes { get; set; } = [];
 }
