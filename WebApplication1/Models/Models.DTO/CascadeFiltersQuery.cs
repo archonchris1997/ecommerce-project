e42d0ce76dd15b0440ac640b2f395ceffc296189
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-24 22:47:01 +0200

    criacao do CascadeFiltersQuery

--- WebApplication1/Models/Models.DTO/CascadeFiltersQuery.cs
+++ WebApplication1/Models/Models.DTO/CascadeFiltersQuery.cs
@@ -2,5 +2,12 @@ namespace WebApplication1.Models.Models.DTO;
 
 public class CascadeFiltersQuery
 {
-    
+    public string? Categoria { get; set; }
+    public string? Marca { get; set; }
+    public string? Modelo { get; set; }
+    public int? Ano { get; set; }
+    public string? Cor { get; set; }
+    public int? ArmazenamentoGB { get; set; }
+    public int? RamGB { get; set; }
+    public string? Condicao { get; set; }
 }
