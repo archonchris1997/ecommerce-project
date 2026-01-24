b364c8b26eb2f3706bb406076e6e47a33f0bffce
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-24 20:58:05 +0200

    .

--- WebApplication1/Controllers/ProdutosController.cs
+++ WebApplication1/Controllers/ProdutosController.cs
@@ -9,5 +9,18 @@ using WebApplication1.Data;
 [Route("api/produtos")]
 public class ProdutosController:ControllerBase
 {
+    private readonly AppDbContext _db;
+    public ProdutosController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken ct)
+    {
+        var produtos = await _db.Produtos.AsNoTracking().ToListAsync(ct);
+        return Ok(produtos);
+    }
+    
     
 }
