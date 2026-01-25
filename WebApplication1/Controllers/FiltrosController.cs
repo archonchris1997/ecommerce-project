e491be5a242ced0e8d49faae06c1c67dadc4feed
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-01-25 12:59:57 +0200

    Vou meter tudo no Controller

--- WebApplication1/Controllers/FiltrosController.cs
+++ WebApplication1/Controllers/FiltrosController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models.Models.DTO;
-using WebApplication1.Services;
 
 namespace WebApplication1.Controllers;
 using Microsoft.EntityFrameworkCore;
@@ -11,9 +10,6 @@ using WebApplication1.Data;
 [Route("api/filtros")]
 public class FiltrosController: ControllerBase
 {
-    private readonly FiltersService _service;
-    
-    
     
 
 
